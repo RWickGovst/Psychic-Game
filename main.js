3535208361b4e623e320dd9073b386ecb66dd24e
Triumph <archie200@icloud.com>
2019-06-01 19:09:37 -0500

    changes to js file

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
 var letter='s';
+var wins=0;
+var losses=0;
+// var guessesLeft;
+// var guessesMade;
 
 var myLetter = prompt("Try to guess my secret letter: ");
-var myLet = Letter(myLetter);
-
+var myLet = name(myLetter);
+{/* <input type="text" onkeydown="myFunction()"></input> */}
 if(myLet === letter){
 
   alert("Great guess");
+  wins++;
 }
 else{
   alert("Not Quite!");
+  losses++;
+  // guesses left
+  // guesses made
 }
